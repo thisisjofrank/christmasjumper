8e5f7c546b335ce5f2bd897beda3f17100c49592
Jo Franchetti <jofranchetti@gmail.com>
2019-11-13 10:23:33 +0000

    arduino all the things

---
+++ app/jumper/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let icon = JSON.stringify("icon");
+
+function App() {
+  return (
+    <div>
+      {icon}
+    </div>
+  );
+}
+
+export default App;
